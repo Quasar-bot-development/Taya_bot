cc91beb88d8a6de7a1bcb33e8b72e941d879e6a8
Aleksey_Pahalev <apahalev@gmail.com>
2024-05-14 21:53:20 +0400

    Update db.py

--- db.py
+++ db.py
@@ -3,11 +3,19 @@ import sqlite3 as sql
 connection = sql.connect('./User_db.db') # Открытие бд
 cursor = connection.cursor()
 
+cursor.execute('''DROP TABLE IF EXISTS User''')
 cursor.execute('''
-            CREATE TABLE IF NOT EXISTS last_message_time (
+            CREATE TABLE IF NOT EXISTS User (
                 id INTEGER PRIMARY KEY,
-                time INTEGER,
-                chat_id INTEGER
+                name TEXT,
+                age INTEGER,
+                phone_number INTEGER,
+                telegram_name TEXT,
+                request TEXT,
+                CURRENT_TIME TEXT,
+                IS_HOMEWORK_DONE BOOLEAN,
+                user_step INTEGER,
+                telegram_id TEXT
             )
         ''')
 
